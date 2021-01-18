e5106d309def92a8aee8080b9e4e57529ecccd15
Fabian Rost <fabian.rost@tu-dresden.de>
2021-01-18 18:48:35 +0100

    skip image test

--- .travis/build.sh
+++ .travis/build.sh
@@ -116,8 +116,8 @@ if [ -f "$recipe" ]; then
 
         # Example testing using run (you could also use test command)
 
-        echo "Testing the image... Marco!"
-        singularity exec $imagefile echo "Polo!"
+        # echo "Testing the image... Marco!"
+        # singularity exec $imagefile echo "Polo!"
 
         # Example sregistry commands to push to endpoints
 
