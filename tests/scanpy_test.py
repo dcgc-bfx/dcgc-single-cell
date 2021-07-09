ed8ae21948801bac6a10728b6779429ae77d23d8
Fabian Rost <fabian.rost@tu-dresden.de>
2021-07-09 13:32:44 +0200

    anndata2ri test

--- tests/scanpy_test.py
+++ tests/scanpy_test.py
@@ -1,7 +1,9 @@
 import scanpy as sc
+import anndata2ri
 
 adata = sc.datasets.blobs()
 
+# basic pipeline
 sc.pp.highly_variable_genes(adata)
 sc.pp.pca(adata)
 sc.pp.neighbors(adata)
@@ -10,5 +12,10 @@ sc.tl.leiden(adata)
 sc.tl.rank_genes_groups(adata, 'leiden')
 
 sc.pl.umap(adata)
+
+# anndata2ri
+adata = sc.datasets.pbmc3k()
+anndata2ri.py2rpy(adata)
+
 
 sc.logging.print_versions()
